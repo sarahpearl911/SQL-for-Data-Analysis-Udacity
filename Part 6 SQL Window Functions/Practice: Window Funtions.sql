bfae40114121dee06383388b41c7544b4b0513ad
Sarah Pearl <106031283+sarahpearl911@users.noreply.github.com>
2022-09-07 15:44:51 -0700

    Create Practice: Window Funtions.sql

---
+++ Part 6 SQL Window Functions/Practice: Window Funtions.sql
@@ -0,0 +1,12 @@
+--#1 Create a running total of standard_amt_usd (in the orders table) over order time with no date truncation. Your final table should have two columns: one with the amount being added for each new row, and a second with the running total.
+
+SELECT standard_amt_usd, 
+       SUM(standard_amt_usd) OVER (ORDER BY occurred_at) running_total
+FROM orders
+
+--#2 Now, modify your query from the previous quiz to include partitions. Still create a running total of standard_amt_usd (in the orders table) over order time, but this time, date truncate occurred_at by year and partition by that same year-truncated occurred_at variable. Your final table should have three columns: One with the amount being added for each row, one for the truncated date, and a final column with the running total within each year.
+
+SELECT standard_amt_usd, 
+       DATE_TRUNC('year', occurred_at) order_year, 
+       SUM(standard_amt_usd) OVER (PARTITION BY DATE_TRUNC('year', occurred_at) ORDER BY occurred_at) running_total
+FROM orders
