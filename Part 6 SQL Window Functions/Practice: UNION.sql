3a789fa47ebbe89413f161bfbf809de6eb4fbd03
Sarah Pearl <106031283+sarahpearl911@users.noreply.github.com>
2022-09-19 13:41:01 -0700

    Update Practice: UNION.sql

--- Part 6 SQL Window Functions/Practice: UNION.sql
+++ Part 6 SQL Window Functions/Practice: UNION.sql
@@ -19,11 +19,12 @@ WHERE name = 'Disney'
 --#3 Perform the union in your first query (under the Appending Data via UNION header) in a common table expression and name it double_accounts. Then do a COUNT the number of times a name appears in the double_accounts table. If you do this correctly, your query results should have a count of 2 for each name.
 
 WITH double_accounts AS (
-  SELECT *
-FROM accounts
-UNION ALL
-SELECT *
-FROM accounts)
+                        SELECT *
+                        FROM accounts
+                        UNION ALL
+                        SELECT *
+                        FROM accounts
+                        )
 
 SELECT name, COUNT(name)
 FROM double_accounts
